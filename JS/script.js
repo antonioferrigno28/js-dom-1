d51d8f9bcef3eddbbde95a8b26337f2fd11c5172
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-22 15:51:57 +0200

    Es DONE! (BONUS DONE)

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,18 @@
 // Seleziona il bottone e l'immagine
 const button = document.getElementById("changeLampBtn");
 const image = document.getElementById("lamp");
+const textLight = document.getElementById("text-lamp");
 
 button.addEventListener("click", function () {
-  // Controlla quale immagine è attualmente mostrata
   if (image.src.includes("white_lamp.png")) {
     image.src = "./img/yellow_lamp.png";
   } else {
     image.src = "./img/white_lamp.png";
+  }
+
+  if (button.textContent.includes("Accendi")) {
+    button.textContent = "Spegni";
+  } else {
+    button.textContent = "Accendi";
   }
 });
