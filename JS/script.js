aea5c228c798ab92f7dc173f458dfc39201e3a9c
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-22 14:38:32 +0200

    Done! (missing bonus)

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
 // Seleziona il bottone e l'immagine
 const button = document.getElementById("changeLampBtn");
 const image = document.getElementById("lamp");
+
+button.addEventListener("click", function () {
+  // Controlla quale immagine è attualmente mostrata
+  if (image.src.includes("white_lamp.png")) {
+    image.src = "./img/yellow_lamp.png";
+  } else {
+    image.src = "./img/white_lamp.png";
+  }
+});
